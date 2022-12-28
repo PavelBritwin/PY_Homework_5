35c507c5d1586a5d3841b2df7a98fc614a87169f
Pavel Anokhin <anohinpa@gmail.com>
2022-12-29 00:23:25 +0300

    Task2

--- Task_2.py
+++ Task_2.py
@@ -1,17 +1,25 @@
 import random
-
-sweets = 100
-step = 28
-player = random.randrange(1, 3)
+sweets = 89
+step = 0
+players = ['Компьютер', 'Игрок']
+random.shuffle(players)
+currentPlayer = players[0]
 while sweets > 28:
-    step = int(input(f'Ход игрока {player}. Введите количество конфет: '))
-    if (0 > step or step > 28):
-        print('Нарушение правил игры! Количество конфет, взятых за один ход должно быть в пределах: 0 < step < 29')
-        continue
+    if (currentPlayer == 'Игрок'):
+        step = int(input(f'Ходит {currentPlayer}. Введите количество конфет: '))
+        if (0 > step or step > 28):
+            print('Нарушение правил игры! Количество конфет, взятых за один ход должно быть в пределах: 0 < step < 29')
+            continue
+    else:
+        if sweets % 29 == 0:
+            step = random.randint(1, 28)
+        else:
+            step = sweets % 29
+        print(f'Ходит {currentPlayer}: взял {step} конфет')
     sweets -= step
     print(f'Осталось конфет: {sweets}')
-    if player == 1:
-        player = 2
+    if currentPlayer == 'Компьютер':
+        currentPlayer = 'Игрок'
     else:
-        player = 1
-print(f'Игрок {player} забирает последние конфеты, он победил!')
+        currentPlayer = 'Компьютер'
+print(f'Игрок {currentPlayer} забирает последние конфеты, он победил!')
